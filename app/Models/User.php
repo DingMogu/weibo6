e9a9cbe83a248b5ca66eee5955d2c5882fe2dcf0
rui <1975261197@qq.com>
2021-05-10 08:48:02 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    //显示当前用户发布的微博
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 }
