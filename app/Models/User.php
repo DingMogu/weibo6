eed0c6436751d3981720b23393d458ddfd2bf770
rui <1975261197@qq.com>
2021-05-10 14:51:00 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,10 +59,14 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    //显示当前用户发布的微博
+    //显示当前用户发布的微博 (关注的用户)
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
     }
 
     //粉丝列表
