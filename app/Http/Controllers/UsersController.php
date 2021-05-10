a697fd1fc8bb89f425114240d2096a5d2cbb96da
rui <1975261197@qq.com>
2021-05-10 03:26:11 +0000

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,30 @@ class UsersController extends Controller
         session()->flash('success','欢迎,您将在这里开启一段新的旅程');
         return redirect()->route('users.show',[$user]);
     }
+
+    //编辑用户个人信息页面
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    //编辑资料提交验证
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data= [];
+        $data['name'] = $request->name;
+        if($request->password)
+        {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新完成');
+        return redirect()->route('users.show',$user);
+    }
 }
