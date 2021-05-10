21a3aa43219a0de47a1c58085a2e6ca9ad109cf2
rui <1975261197@qq.com>
2021-05-10 14:50:01 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,7 @@ Route::resource('statuses','StatusesController',['only' => ['store','destroy']])
 //关注 粉丝列表
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//关注 取消关注
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
